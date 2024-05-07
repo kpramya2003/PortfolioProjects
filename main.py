6fcbe892ef6e6cc8b6ca63ae560291e28dc46315
kpramya2003 <159487426+kpramya2003@users.noreply.github.com>
2024-05-07 11:16:27 -0500

    Add files via upload
    Morse to Text

--- main.py
+++ main.py
@@ -1,66 +1,46 @@
-print('welcome to TIC TAC TOE')
-pos_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    "1": "•----",
+    "2": "••---",
+    "3": "•••--",
+    "4": "••••-",
+    "5": "•••••",
+    "6": "-••••",
+    "7": "--•••",
+    "8": "---••",
+    "9": "----•",
+    "0": "-----",
+    ' ': '/'
+}
 
 
-def display_board():
-    result = ((f'{pos_num[0]}    |   {pos_num[1]}    |   {pos_num[2]}\n' + f'----------------------\n' +
-              f'{pos_num[3]}    |   {pos_num[4]}    |   {pos_num[5]}\n') + f'----------------------\n' +
-              f'{pos_num[6]}    |   {pos_num[7]}    |   {pos_num[8]}\n')
-    print(result)
-
-display_board()
-
-game = True
-player1_turn = True
-
-player_1 = player_2 = []
-
-while game:
-    try:
-        if player1_turn:
-            n = int(input('Enter number position you want to put "X" :'))
-            if n in player_2:
-                print('number position already filled by player 2')
-            else:
-                player_1.append(n)
-                pos_num[n-1] = 'X'
-                display_board()
-                player1_turn = False
-
-        else:
-            n = int(input('Enter number position you want to put "0" :'))
-            if n in player_1:
-                print('number position already filled by player 1')
-            else:
-                player_2.append(n)
-                pos_num[n - 1] = '0'
-
-                player1_turn = True
-                display_board()
-    except ValueError:
-        print('Not a  valid number')
-        game = False
-    except IndexError:
-        print('Number must be between 1 to 9')
-        game = False
-
-    if pos_num[0]==pos_num[1]==pos_num[2] or pos_num[3]==pos_num[4]==pos_num[5] or pos_num[6]==pos_num[7]==pos_num[8] or pos_num[0]==pos_num[3]==pos_num[6] or pos_num[1]==pos_num[4]==pos_num[7] or pos_num[2]==pos_num[5]==pos_num[8] or pos_num[0]==pos_num[4]==pos_num[8] or pos_num[2]==pos_num[4]==pos_num[6]:
-        if player1_turn:
-            print("Player 2 Won the game")
-            game = False
-            player1_turn = False
-        else:
-            print("Player 1 Won the game")
-            game = False
-            player1_turn = False
-    if len(player_1) == 9:
-        print("It is draw")
-        game = False
-        player1_turn = False
+def text_to_morse(text_code):
+    output = [MORSE_CODE_DICT.get(char) for char in text_code]
+    return output
 
 
+def morse_to_text(morse_code):
+    output = ''
+    for char in morse_code.split():
+        for (key, value) in MORSE_CODE_DICT.items():
+            if char == value:
+                output += key
+                break
+    return output.strip()
 
 
-
-
+option = int(input('Enter your options  1.Text to Morse 2.Morse to text \n'))
+if option == 1:
+    user_input = input("Enter the string and number you wish to convert into Morse_code characters\n").upper()
+    morse = text_to_morse(user_input)
+    print(morse)
+elif option == 2:
+    user_input = input("Enter the Morse code character you wish to convert into text between spaces\n")
+    text = morse_to_text(user_input)
+    print(text)
 
